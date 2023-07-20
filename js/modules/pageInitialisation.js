5dac52ac2cd591ecbff5a282660633ef8b70a236
AndriyStechenko <34598154+AndriyStechenko@users.noreply.github.com>
2023-07-20 15:44:51 +0200

    monir fix

--- js/modules/pageInitialisation.js
+++ js/modules/pageInitialisation.js
@@ -1,19 +1,15 @@
 import {readFromStorage} from './storage.js';
 import {showRollDiceBtn, showEndTurnBnt, makeDiceSectionVisible, createResultTable, showScoreTable} from './callbacks.js';
-import {playerIsKnown} from './playerNameGetterModule.js';
-import {initializeScoreTable} from './scoreTableInitialization.js';
-import {drawAllDices, drawEmptyDices, drawDicesFromLastTurn} from './dicesDrawer.js';
+import {drawEmptyDices, drawDicesFromLastTurn} from './dicesDrawer.js';
 
 function initializePage() {
   const currentScoreTable = readFromStorage('currentScoreTable');
   if (currentScoreTable) {
-    // alert('bla bla bla');
     showRollDiceBtn();
     showEndTurnBnt();
     makeDiceSectionVisible();
     drawEmptyDices();
     drawDicesFromLastTurn();
-    // drawAllDices();
     showScoreTable();
     createResultTable();
   }
