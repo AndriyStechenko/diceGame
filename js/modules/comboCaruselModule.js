5c4cb249a7441499e9ecdaf21581a7a3c6f35658
AndriyStechenko <34598154+AndriyStechenko@users.noreply.github.com>
2023-08-07 19:42:47 +0200

    some more fixes

--- js/modules/comboCaruselModule.js
+++ js/modules/comboCaruselModule.js
@@ -29,37 +29,95 @@ function turnOffBorderLight(carouselElement) {
 }
 
 function lightDices(carouselElement) {
-  if (carouselElement.target.id === 'oneCombination') {
-    const diceElements = document.querySelectorAll('.dice_pic_1');
-    diceElements.forEach((diceElement) => {
-      diceElement.classList.add('dice-on-hold-back-light');
-    });
+  const carouselElementId = carouselElement.target.id;
+  const combinationToDiceClassMap = {
+    'oneCombination': '.dice_pic_1',
+    'twoCombination': '.dice_pic_2',
+    'threeCombination': '.dice_pic_3',
+    'fourCombination': '.dice_pic_4',
+    'fiveCombination': '.dice_pic_5',
+    'sixCombination': '.dice_pic_6',
+    'smallStritCombination': [1, 2, 3, 4, 5],
+  };
+
+  if (carouselElementId === 'smallStritCombination') {
+    console.log('Finally smallStritCombination');
+  } else {
+    if (carouselElementId in combinationToDiceClassMap) {
+      const diceElements = document.querySelectorAll(combinationToDiceClassMap[carouselElementId]);
+      _diceLighter(diceElements);
+    }
   }
 }
 
+function _diceLighter(diceElements) {
+  diceElements.forEach((diceElement) => {
+    diceElement.classList.add('dice-on-hold-back-light');
+  });
+}
+
 function lightOffDices(carouselElement) {
-  if (carouselElement.target.id === 'oneCombination' && !optionClicked) {
-    const diceElements = document.querySelectorAll('.dice_pic_1');
-    diceElements.forEach((diceElement) => {
-      diceElement.classList.remove('dice-on-hold-back-light');
-    });
+  const carouselElementId = carouselElement.target.id;
+  const combinationToDiceClassMap = {
+    'oneCombination': '.dice_pic_1',
+    'twoCombination': '.dice_pic_2',
+    'threeCombination': '.dice_pic_3',
+    'fourCombination': '.dice_pic_4',
+    'fiveCombination': '.dice_pic_5',
+    'sixCombination': '.dice_pic_6',
+    'smallStritCombination': [1, 2, 3, 4, 5],
+  };
+
+  if (carouselElementId in combinationToDiceClassMap && !optionClicked) {
+    const diceElements = document.querySelectorAll(combinationToDiceClassMap[carouselElementId]);
+    _diceLighterOff(diceElements);
   }
   optionClicked = false;
 }
 
+function _diceLighterOff(diceElements) {
+  diceElements.forEach((diceElement) => {
+    diceElement.classList.remove('dice-on-hold-back-light');
+  });
+}
+
 function makeDiceOnHoldInTurn() {
   const elementId = event.target.id;
+  console.log(elementId);
   const currentTurn = readFromStorage('currentTurn');
-  if (elementId === 'oneCombination') {
-    for (const dice of currentTurn.dices) {
-      if (dice.value === 1) {
-        dice.onHold = true;
-        currentTurn.usedCombo = elementId;
-      }
+  const combinationMap = {
+    'oneCombination': 1,
+    'twoCombination': 2,
+    'threeCombination': 3,
+    'fourCombination': 4,
+    'fiveCombination': 5,
+    'sixCombination': 6,
+    'smallStritCombination': [1, 2, 3, 4, 5],
+  };
+
+  const diceValueToHold = combinationMap[elementId];
+  console.log(diceValueToHold);
+
+  for (const dice of currentTurn.dices) {
+    if (dice.value === diceValueToHold) {
+      dice.onHold = true;
+      currentTurn.usedCombo = elementId;
+      currentTurn.dicesSum += dice.value;
     }
   }
+
   saveToStorage('currentTurn', currentTurn);
 }
+
+// function _checkSmallStritCombination(obj) {
+//   const diceValues = obj.dices.map((dice) => dice.value);
+//   const smallStritValues = [1, 2, 3, 4, 5];
+//   const isSmallStritCombination = smallStritValues.every((value) => diceValues.includes(value));
+
+//   if (isSmallStritCombination) {
+//     console.log('JaPerdole this is ', 'smallStritCombination');
+//   }
+// }
 
 
 // Add event listener to each option
